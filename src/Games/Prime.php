db87d1740417a63b62ce4081a8f7bc42c902628c
Alexsey-VR <vityaz.iss@yandex.ru>
2025-04-20 15:23:11 +0000

    removed namespace from bin scripts

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -4,10 +4,10 @@ namespace BrainGames\Games;
 
 function isPrimeNumber(int $number): bool
 {
-    if (($number === 1) || ($number === 2)) {
+    if (($number > 0) && ($number < 4)) {
         return true;
     }
-    return (((3 ** $number) % $number) === 3) ? true : false;
+    return (((3 ** $number) % $number === 3) && ((2 ** ($number - 1)) % $number === 1)) ? true : false;
 }
 
 function runPrime(): void
@@ -15,7 +15,7 @@ function runPrime(): void
     $name = greeting();
     print_r("Answer \"yes\" if given number is prime. Otherwise answer \"no\".\n");
     for ($i = 0; $i < MAX_GAME_ROUNDS; $i++) {
-        $question = rand(1, 30);
+        $question = rand(1, 20);
         $answer = makeQuestionGetAnswer((string)$question);
         $result = isPrimeNumber($question) ? "yes" : "no";
         if (answerIsTrue($result, $answer)) {
