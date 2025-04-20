56bbf23cc03171e5e125b5d7bc2201566a464a26
Alexsey-VR <vityaz.iss@yandex.ru>
2025-04-20 16:47:11 +0000

    use cli library in game engine

--- src/Engine.php
+++ src/Engine.php
@@ -3,14 +3,8 @@
 namespace BrainGames\Games;
 
 use function cli\line;
+
 use function cli\prompt;
-/*
-function cli(): void
-{
-    line("Welcome to the Brain Games!");
-    $name = prompt("May I have your name?");
-    line("Hello, %s!", $name);
-*/
 
 const MAX_GAME_ROUNDS = 3;
 
