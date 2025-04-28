e0874b45441c084f9b60b1c174ca02b72de29e1b
Alexsey-VR <vityaz.iss@yandex.ru>
2025-04-28 15:45:24 +0000

    add brain game progression to game engine

--- src/Games/Progression.php
+++ src/Games/Progression.php
@@ -1,34 +1,26 @@
 <?php
 
-namespace BrainGames\Games;
+namespace BrainGames\Games\Progression;
 
-use function BrainGames\Games\answerIsTrue;
-use function BrainGames\Games\makeQuestionGetAnswer;
-use function BrainGames\Games\greeting;
-use function cli\line;
+use function BrainGames\Engine\runEngine;
 
-use const BrainGames\Games\MAX_GAME_ROUNDS;
+use const BrainGames\Engine\MAX_GAME_ROUNDS;
 
 function runProgression(): void
 {
-    $name = showGreeting();
-    print_r("What number is missing in the progression?\n");
+    $flow['description'] = "What number is missing in the progression?";
+    $flow['questionData'] = [];
+    $flow['trueResult'] = [];
     for ($i = 0; $i < MAX_GAME_ROUNDS; $i++) {
         $initValue = rand(1, 20);
         $stepValue = rand(1, 5);
         $range = range($initValue, $initValue + 10 * $stepValue, $stepValue);
         $gapValue = rand(0, 9);
-        $result = $range[$gapValue];
+        $trueResult = $range[$gapValue];
         $range[$gapValue] = "..";
-        $question = implode(" ", $range);
-        $answer = makeQuestionGetAnswer($question);
-        if (answerIsTrue((string)$result, $answer)) {
-            line("Correct!");
-        } else {
-            line("'{$answer}' is wrong answer ;(. Correct answer was '{$result}'.");
-            line("Let's try again, {$name}!");
-            return;
-        }
+        $flow['questionData'][] = implode(" ", $range);
+        $flow['trueResult'][] = $trueResult;
     }
-    line("Congratulations, {$name}!");
+
+    runEngine($flow);
 }
