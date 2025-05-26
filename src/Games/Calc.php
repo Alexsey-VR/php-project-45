01c094f0df42f9c439054ca3fd90bab0cf40385f
Alexsey-VR <vityaz.iss@yandex.ru>
2025-05-26 16:32:17 +0000

    remove try catch from brain calc game

--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -6,20 +6,20 @@ use function BrainGames\Engine\runGame;
 
 use const BrainGames\Engine\GAMES_COUNT;
 
+const OPERATORS = ['+', '-', '*'];
+
 function computeOperation(
     int $leftOperand,
     int $rightOperand,
     string $operator
 ): int {
     switch ($operator) {
-        case '+':
+        case OPERATORS[0]:
             return ($leftOperand + $rightOperand);
-        case '-':
+        case OPERATORS[1]:
             return ($leftOperand - $rightOperand);
-        case '*':
+        case OPERATORS[2]:
             return ($leftOperand * $rightOperand);
-        default:
-            throw new \Exception("Operation '{$operator}' not permitted\n");
     }
 }
 
@@ -29,20 +29,14 @@ function runCalc(): void
     $flowSteps = [];
     $flowSteps['questionData'] = [];
     $flowSteps['trueResult'] = [];
-    try {
-        $collection = ['+', '-', '*'];
-        $flowSteps = array_map(function ($operator) {
-            $a = rand(1, 100);
-            $b = rand(1, 100);
-            return [
-                'questionData' => "{$a} {$operator} {$b}",
-                'trueResult' => computeOperation($a, $b, $operator)
-            ];
-        }, $collection);
-    } catch (\Exception $e) {
-        print_r($e->getMessage());
-        return;
-    }
+    $flowSteps = array_map(function ($operator) {
+        $a = rand(1, 100);
+        $b = rand(1, 100);
+        return [
+            'questionData' => "{$a} {$operator} {$b}",
+            'trueResult' => computeOperation($a, $b, $operator)
+        ];
+    }, OPERATORS);
 
     runGame($description, $flowSteps);
 }
